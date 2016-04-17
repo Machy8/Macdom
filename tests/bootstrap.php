6b39b897528a629df1ef9e6e08ae3125044b5c00
Vladimír Macháček <8machy@seznam.cz>
2016-04-17 15:58:14 +0200

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,6 +4,3 @@ require __DIR__ . '/../../../autoload.php';
 require __DIR__ . '/../src/Loader.php';
 
 $o = new Machy8\Macdom\Loader;
-
-\Tracy\Debugger::enable(\Tracy\Debugger::DETECT, __DIR__."/../myLog");
-
