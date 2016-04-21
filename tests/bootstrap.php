a0135513590e555fedf99196419d76b018f21dfe
Vladimír Macháček <8machy@seznam.cz>
2016-04-21 17:02:08 +0200

    Edited bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,11 +3,4 @@
 require __DIR__ . '/../../../autoload.php';
 require __DIR__ . '/../src/Loaders/Loader.php';
 
-<<<<<<< HEAD
 $o = new Machy8\Macdom\Loaders\Loader;
-
-\Tracy\Debugger::enable(\Tracy\Debugger::DETECT, __DIR__."/../myLog");
-
-=======
-$o = new Machy8\Macdom\Loader;
->>>>>>> origin/develop
