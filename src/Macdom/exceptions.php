1e2392e6e76ee4f8592f61c142a333579a6921a9
Vladimír Macháček <8machy@seznam.cz>
2017-05-14 00:01:20 +0200

    - v3.0 - Created exceptions.php

---
+++ src/Macdom/exceptions.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ *
+ * This file is part of the Macdom
+ *
+ * Copyright (c) 2015-2017 Vladimír Macháček
+ *
+ * For the full copyright and license information, please view the file license.md
+ * that was distributed with this source code.
+ *
+ */
+
+declare(strict_types = 1);
+
+namespace Macdom;
+
+
+class CompileException extends \Exception
+{
+
+}
+
+
+class SetupException extends \Exception
+{
+
+}
