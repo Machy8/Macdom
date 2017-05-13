744aff43c6ee51142e37150677d930729d4e2428
Vladimír Macháček <8machy@seznam.cz>
2017-05-13 23:59:10 +0200

    - v3.0 - Created CoreElementsBooleanAttributes.php

---
+++ src/Macdom/Elements/CoreElementsBooleanAttributes.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ *
+ * This file is part of the Macdom
+ *
+ * Copyright (c) 2015-2017 Vladimír Macháček
+ *
+ * For the full copyright and license information, please view the file license.md
+ * that was distributed with this source code.
+ *
+ */
+
+declare(strict_types = 1);
+
+namespace Macdom\Elements;
+
+use Macdom\Compiler;
+
+
+final class CoreElementsBooleanAttributes extends AbstractElementsManager
+{
+
+	public static function install(Compiler $compiler): void
+	{
+		$elementsManager = new static($compiler);
+
+		$elementsManager
+			->addBooleanAttribute('allowfullscreen')
+			->addBooleanAttribute('autofocus')
+			->addBooleanAttribute('autoplay')
+			->addBooleanAttribute('async')
+			->addBooleanAttribute('contenteditable')
+			->addBooleanAttribute('controls')
+			->addBooleanAttribute('default')
+			->addBooleanAttribute('defer')
+			->addBooleanAttribute('disabled')
+			->addBooleanAttribute('draggable')
+			->addBooleanAttribute('formnovalidate')
+			->addBooleanAttribute('hidden')
+			->addBooleanAttribute('checked')
+			->addBooleanAttribute('ismap')
+			->addBooleanAttribute('loop')
+			->addBooleanAttribute('multiple')
+			->addBooleanAttribute('muted')
+			->addBooleanAttribute('open')
+			->addBooleanAttribute('readonly')
+			->addBooleanAttribute('required')
+			->addBooleanAttribute('selected')
+			->addBooleanAttribute('spellcheck');
+	}
+
+}
