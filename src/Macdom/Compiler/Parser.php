bfb38a972f6cd3f0ed1e539455a5cad5b8262593
Vladimír Macháček <8machy@seznam.cz>
2017-06-30 17:55:57 +0200

    - v3.0 - Updated Parser

--- src/Macdom/Compiler/Parser.php
+++ src/Macdom/Compiler/Parser.php
@@ -21,26 +21,28 @@ final class Parser
 
 	const
 		TOKEN_TYPES_MATCH_ORDER = [
-			Token::UNFORMABLE_TEXT,
 			Token::ELEMENT,
-			Token::ELEMENT_OPEN_TAG,
-			Token::ELEMENT_CLOSE_TAG,
 			Token::MACRO,
 			Token::REPLICATOR_OPEN_TAG,
 			Token::REPLICATOR_CLOSE_TAG,
-			Token::REPLICATOR_REPLICANT,
+			Token::REPLICATOR_REPLICA,
 			Token::TEXT
 		],
 
 		REPLICATOR_REGISTER_PREFIX = '#',
-
-		SKIPPED_AREA_INDENTED_BLOCK_MODE = 'skippedAreaIndentedBlock',
-		SKIPPED_AREA_TAGGED_BLOCK_MODE = 'skippedAreaTaggedBlock',
-		SKIP_TAG = 'SKIP',
-
 		REPLICATOR_PREFIX = '@',
 		REPLICATOR_OPEN_TAG_RE = '/^' . self::REPLICATOR_PREFIX . '(?<keyword>[\S]*)/',
-		REPLICATOR_CLOSE_TAG_RE = '/^\/' . self::REPLICATOR_PREFIX . '(?<keyword>[\S]*)/';
+		REPLICATOR_CLOSE_TAG_RE = '/^\/' . self::REPLICATOR_PREFIX . '(?<keyword>[\S]*)/',
+
+		SKIP_TAG = 'macdom-off',
+
+		CODE_PLACEHOLDER_NAMESPACE_PREFIX = 'codePlaceholder_',
+		CODE_PLACEHOLDER_RE = '/' . self::CODE_PLACEHOLDER_NAMESPACE_PREFIX . '\d+/';
+
+	/**
+	 * @var array
+	 */
+	private $codePlaceholdersRegularExpression = [];
 
 	/**
 	 * @var Compiler
@@ -72,68 +74,10 @@ final class Parser
 	 */
 	private $replicatorRegister = [];
 
-	/**
-	 * @var array
-	 */
-	private $skippedArea;
-
 
 	public function __construct(Compiler $compiler)
 	{
 		$this->compiler = $compiler;
-	}
-
-
-	public function analyzeString(string $string): array
-	{
-		$type = Token::TEXT;
-		$keyword = Helpers::getFirstWord($string);
-		$settings = NULL;
-
-		if ($this->compiler->findElement($keyword)) {
-			$type = Token::ELEMENT;
-
-		} elseif ($this->compiler->findMacro($keyword)) {
-			$type = Token::MACRO;
-
-		} elseif (Helpers::matchElementOpenTag($string, $matches, $this->compiler->getElementsCustomOpenTags())
-			&& $this->compiler->findElement($matches['element'])
-		) {
-			$keyword = $matches['element'];
-			$type = Token::ELEMENT_OPEN_TAG;
-
-		} elseif (Helpers::matchElementCloseTag($string, $matches, $this->compiler->getElementsCustomCloseTags())) {
-			$type = Token::ELEMENT_CLOSE_TAG;
-
-		} elseif (preg_match(self::REPLICATOR_OPEN_TAG_RE, $keyword, $matches)) {
-			$keyword = $matches['keyword'];
-			$type = Token::REPLICATOR_OPEN_TAG;
-
-		} elseif (preg_match(self::REPLICATOR_CLOSE_TAG_RE, $keyword, $matches)) {
-			$keyword = $matches['keyword'];
-			$type = Token::REPLICATOR_CLOSE_TAG;
-		}
-
-		$typeIsElement = in_array($type, [Token::ELEMENT, Token::ELEMENT_OPEN_TAG], TRUE);
-
-		if ($typeIsElement) {
-			$elementSettings = $this->compiler->getElement($keyword);
-
-			$details = [
-				'skipElement' => in_array(Engine::SKIPPED_ELEMENT, $elementSettings),
-				'skipContent' => in_array(Engine::CONTENT_SKIPPED, $elementSettings)
-			];
-
-			if ($this->isElementUnpaired($elementSettings)) {
-				$details[] = Engine::UNPAIRED_ELEMENT;
-			}
-		}
-
-		return [
-			'keyword' => $keyword,
-			'type' => $type,
-			'details' => $details ?? []
-		];
 	}
 
 
@@ -145,13 +89,11 @@ final class Parser
 
 	public function parse(string $input): array
 	{
+		$this->setCodePlaceholdersRegularExpressions();
+
 		$input = str_replace("\r\n", "\n", $input);
 		$this->output = [];
-		$this->skippedArea = [
-			'mode' => [],
-			'indentationLevel' => [],
-			'closeTags' => []
-		];
+		$input = $this->setCodePlaceholders($input, $codePlaceholders);
 
 		foreach (preg_split("/\\r|\\n/", $input, -1, PREG_SPLIT_DELIM_CAPTURE) as $lineNumber => $line) {
 			$this->setProcessedLine($line, $lineNumber);
@@ -163,7 +105,43 @@ final class Parser
 			}
 		}
 
-		return $this->output;
+		return [
+			'tokens' => $this->output,
+			'codePlaceholders' => $codePlaceholders
+		];
+	}
+
+
+	public function setCodePlaceholders(string $string, array &$codePlaceholders = NULL): string
+	{
+		$codePlaceholders = [];
+
+		foreach ($this->codePlaceholdersRegularExpression as $codePlaceholderRe => $surroundByIndentation) {
+			preg_match_all($codePlaceholderRe, $string, $matches, PREG_SET_ORDER);
+
+			foreach ($matches as $match) {
+				$code = end($match);
+
+				if ( ! trim($code)) {
+					continue;
+				}
+
+				$placeholderId = uniqid();
+				$codePlaceholder = self::CODE_PLACEHOLDER_NAMESPACE_PREFIX . $placeholderId;
+
+				if ($surroundByIndentation) {
+					$indentation = $this->indentationMethod === Engine::TABS_INDENTATION ? "\t" : " ";
+					$codePlaceholder = $indentation . $codePlaceholder;
+				}
+
+				$string = preg_replace('/' . preg_quote($code, '/') . '/', $codePlaceholder, $string, 1);
+				$codePlaceholders[trim($codePlaceholder)] = $code;
+			}
+		}
+
+		$string = preg_replace('/<\/?' . self::SKIP_TAG . '>|' . self::SKIP_TAG . '/', '', $string);
+
+		return $string;
 	}
 
 
@@ -189,7 +167,8 @@ final class Parser
 
 	private function getIndentationLevel(string $token): int
 	{
-		Helpers::getIndentation($token, $whitespaceCharacters);
+		$matched = preg_match('/^\s+/', $token, $matches);
+		$whitespaceCharacters = $matched ? $matches[0] : '';
 		$tabsIndentation = preg_match_all('/\t/', $whitespaceCharacters);
 
 		if ($this->indentationMethod === Engine::TABS_INDENTATION) {
@@ -220,7 +199,7 @@ final class Parser
 
 	private function matchElement(): bool
 	{
-		if ($this->processedLine['type'] !== Token::ELEMENT) {
+		if ( ! $this->compiler->findElement($this->processedLine['keyword'])) {
 			return FALSE;
 		}
 
@@ -232,33 +211,9 @@ final class Parser
 	}
 
 
-	private function matchElementCloseTag(): bool
-	{
-		if ($this->processedLine['type'] !== Token::ELEMENT_CLOSE_TAG) {
-			return FALSE;
-		}
-
-		$this->addToken(Token::ELEMENT_CLOSE_TAG);
-
-		return TRUE;
-	}
-
-
-	private function matchElementOpenTag(): bool
-	{
-		if ($this->processedLine['type'] !== Token::ELEMENT_OPEN_TAG) {
-			return FALSE;
-		}
-
-		$this->addToken(Token::ELEMENT_OPEN_TAG);
-
-		return TRUE;
-	}
-
-
 	private function matchMacro(): bool
 	{
-		if ($this->processedLine['type'] !== Token::MACRO) {
+		if ( ! $this->compiler->findMacro($this->processedLine['keyword'])) {
 			return FALSE;
 		}
 
@@ -272,13 +227,14 @@ final class Parser
 
 	private function matchReplicatorCloseTag(): bool
 	{
-		if ($this->processedLine['type'] !== Token::REPLICATOR_CLOSE_TAG) {
+		if ( ! preg_match(self::REPLICATOR_CLOSE_TAG_RE, $this->processedLine['keyword'], $matches)) {
 			return FALSE;
 		}
 
+		$keyword = preg_replace('/^\/@/', '', $this->processedLine['keyword'], 1);
 		$processedLineLevel = $this->processedLine['indentationLevel'];
-		$replicatedLineKey = ! empty($this->processedLine['keyword'])
-			? $this->processedLine['keyword']
+		$replicatedLineKey = (bool) $keyword
+			? $keyword
 			: '#' . $processedLineLevel;
 
 		if (array_key_exists($replicatedLineKey, $this->replicatorRegister)) {
@@ -295,21 +251,19 @@ final class Parser
 
 	private function matchReplicatorOpenTag(): bool
 	{
-		$processedLineKeyword = $this->processedLine['keyword'];
-
-		if ($this->processedLine['type'] !== Token::REPLICATOR_OPEN_TAG) {
+		if ( ! preg_match(self::REPLICATOR_OPEN_TAG_RE, $this->processedLine['keyword'], $matches)) {
 			return FALSE;
 		}
 
-		if ((bool) $processedLineKeyword) {
-			if (substr($processedLineKeyword, 0, 1) === self::REPLICATOR_REGISTER_PREFIX) {
+		if ((bool) $matches['keyword']) {
+			if (substr($matches['keyword'], 0, 1) === self::REPLICATOR_REGISTER_PREFIX) {
 				throw new CompileException(
 					'Replicated line keyword can\'t start with "' . self::REPLICATOR_REGISTER_PREFIX
 					. '" because it is default internal selector on line "' . $this->processedLine['line']
 					. '" near "' . $this->processedLine['keyword'] . '"');
 			}
 
-			$this->replicatorRegister[$processedLineKeyword] = [
+			$this->replicatorRegister[$matches['keyword']] = [
 				'indentationLevel' => $this->processedLine['indentationLevel'],
 				'text' => $this->processedLine['text']
 			];
@@ -324,7 +278,7 @@ final class Parser
 	}
 
 
-	private function matchReplicatorReplicant(): bool
+	private function matchReplicatorReplica(): bool
 	{
 		if ( ! $this->processedLine['text']) {
 			return TRUE;
@@ -336,27 +290,25 @@ final class Parser
 		if (isset($this->replicatorRegister[$processedLineKeyword])
 			&& $processedLineLevel >= $this->replicatorRegister[$processedLineKeyword]['indentationLevel']
 		) {
-			$this->addToken(Token::REPLICATOR_REPLICANT, [
-				'text' => [
-					'replicated' => $this->replicatorRegister[$processedLineKeyword]['text'],
-					'synchronized' => Helpers::removeFirstWord($this->processedLine['text'])
-				]
-			]);
-
-			return TRUE;
+			$replicatedText = $this->replicatorRegister[$processedLineKeyword]['text'];
+			$synchronizedText = Helpers::removeFirstWord($this->processedLine['text']);
 
 		} elseif (isset($this->replicatorRegister[self::REPLICATOR_REGISTER_PREFIX . $processedLineLevel])) {
-			$this->addToken(Token::REPLICATOR_REPLICANT, [
-				'text' => [
-					'replicated' => $this->replicatorRegister[self::REPLICATOR_REGISTER_PREFIX . $processedLineLevel],
-					'synchronized' => $this->processedLine['text']
-				]
-			]);
+			$replicatedText = $this->replicatorRegister[self::REPLICATOR_REGISTER_PREFIX . $processedLineLevel];
+			$synchronizedText = $this->processedLine['text'];
 
-			return TRUE;
+		} else {
+			return FALSE;
 		}
 
-		return FALSE;
+		$this->addToken(Token::REPLICATOR_REPLICA, [
+			'text' => [
+				'replicated' => Helpers::removeFirstWord($replicatedText),
+				'synchronized' => $synchronizedText
+			]
+		]);
+
+		return TRUE;
 	}
 
 
@@ -368,87 +320,30 @@ final class Parser
 	}
 
 
-	private function matchUnformableText(): bool
+	private function setCodePlaceholdersRegularExpressions()
 	{
-		$processedLineKeyword = $this->processedLine['keyword'];
-		$processedLineType = $this->processedLine['type'];
-		$processedLineDetails = $this->processedLine['details'];
+		$indentation = $this->indentationMethod === Engine::TABS_INDENTATION
+			? '\t'
+			: ' {' . $this->indentationSize . '}*';
 
-		if ( ! $this->skippedArea['mode']) {
-			if ($processedLineKeyword === self::SKIP_TAG) {
-				$this->setSkippedArea(self::SKIPPED_AREA_INDENTED_BLOCK_MODE);
-
-				return TRUE;
-
-			} elseif ($processedLineType === Token::ELEMENT_OPEN_TAG) {
-				$elementSkipped = $processedLineDetails['skipElement'];
-
-				if ($elementSkipped && isset($processedLineDetails['closeTags'])) {
-					$this->setSkippedArea(self::SKIPPED_AREA_TAGGED_BLOCK_MODE, $processedLineDetails['closeTags']);
-
-				} elseif ($elementSkipped || $processedLineDetails['skipContent']) {
-					$this->setSkippedArea(self::SKIPPED_AREA_TAGGED_BLOCK_MODE, '/\<\/{1}[^\/ ?]+\>|[^?< ]*\?\>/');
-				}
-
-			} elseif ($processedLineType === Token::ELEMENT && $processedLineDetails['skipContent']) {
-				$this->setSkippedArea(self::SKIPPED_AREA_INDENTED_BLOCK_MODE);
-			}
-
-		} elseif ($this->skippedArea['mode']) {
-			$closeTags = $this->skippedArea['closeTags'];
-
-			if ($this->skippedArea['mode'] === self::SKIPPED_AREA_TAGGED_BLOCK_MODE && $closeTags
-				&& (is_array($closeTags) && in_array($processedLineKeyword, $closeTags)
-				|| is_string($closeTags) && preg_match($closeTags, $processedLineKeyword))
-				|| $this->skippedArea['mode'] === self::SKIPPED_AREA_INDENTED_BLOCK_MODE
-				&& $this->processedLine['indentationLevel'] < $this->skippedArea['indentationLevel']
-			) {
-				$this->setSkippedArea(NULL);
-
-				return FALSE;
-			}
-
-			$this->addToken(Token::UNFORMABLE_TEXT);
-
-			return TRUE;
-		}
-
-		return FALSE;
+		// Regular expression => indented from left
+		$this->codePlaceholdersRegularExpression = [
+			'/<\?php(?: |\n)(?:.|\n)*\?>/Um' => FALSE, // PHP code
+			'/(' . $indentation . '*)(?<! |\S)(?:' . self::SKIP_TAG . '|code|style|script)((?= ) \S+(?:.*\n\1' . $indentation . '.*)*|((?= *\n)(?:.*\n\1' . $indentation . '.*)+))/' => TRUE, // indented block
+			'/<(' . self::SKIP_TAG . '|code|script|style)(?:[-\w]+)?(?:[^>]+)?>([\s\S]*?)<\/\1>/' => FALSE // tags
+		];
 	}
 
 
 	private function setProcessedLine(string $line, int $lineNumber)
 	{
-		$tokenInfo = $this->analyzeString($line);
-
 		$this->processedLine = [
-			'keyword' => $tokenInfo['keyword'],
-			'type' => $tokenInfo['type'],
-			'details' => $tokenInfo['details'],
+			'keyword' => Helpers::getFirstWord($line),
 			'text' => $line,
 			'line' => $lineNumber + 1,
 		];
 
 		$this->processedLine['indentationLevel'] = $this->getIndentationLevel($line);
-
-	}
-
-
-	/**
-	 * @param string|NULL $mode
-	 * @param string|array|NULL $closeTags
-	 */
-	private function setSkippedArea($mode, $closeTags = NULL)
-	{
-		if ($mode === self::SKIPPED_AREA_INDENTED_BLOCK_MODE) {
-			$indentationLevel = $this->processedLine['indentationLevel'] + 1;
-		}
-
-		$this->skippedArea = [
-			'mode' => $mode,
-			'indentationLevel' => $indentationLevel ?? 0,
-			'closeTags' => $closeTags ?? []
-		];
 	}
 
 }
