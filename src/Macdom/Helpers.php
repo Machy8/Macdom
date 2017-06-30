36dca7159ad8cbcf336b46a1d9fa4d26cfab9d7e
Vladimír Macháček <8machy@seznam.cz>
2017-06-30 18:32:12 +0200

    - v3.0 - Updated Helpers

--- src/Macdom/Helpers.php
+++ src/Macdom/Helpers.php
@@ -19,11 +19,6 @@ namespace Macdom;
 final class Helpers
 {
 
-	const
-		ELEMENT_OPEN_TAG_RE = '/^<(?<element>\S+)(?:[^>]+)?\/?>/',
-		ELEMENT_CLOSE_TAG_RE = '/^<\/[^\>]+>/';
-
-
 	public static function explodeString(string $string): array
 	{
 		return explode(' ', trim($string));
@@ -32,38 +27,15 @@ final class Helpers
 
 	public static function getFirstWord(string $string): string
 	{
-		$keyword = strtok(trim($string), " ");
-
-		return $keyword ? $keyword : '';
-	}
-
-
-	public static function getIndentation(string $string, string &$matches = NULL): bool
-	{
-		$matched = preg_match('/^\s+/', $string, $matches);
-		$matches = $matched ? $matches[0] : '';
-
-		return (bool) $matched;
-	}
-
-
-	public static function matchElementCloseTag(string $string, array &$matches = NULL, string $customTags = NULL): bool
-	{
-		return preg_match(self::ELEMENT_CLOSE_TAG_RE, $string, $matches)
-			|| $customTags && preg_match('/' . $customTags . '/', $string, $matches);
-	}
-
-
-	public static function matchElementOpenTag(string $string, array &$matches = NULL, string $customTags = NULL): bool
-	{
-		return preg_match(self::ELEMENT_OPEN_TAG_RE, $string, $matches)
-			|| $customTags && preg_match('/(?:' . $customTags . ')(?<element>\S+)/', $string, $matches);
+		return preg_match('/^(?<firstWord>\S+)(?:(?=\S+)\s)?/', trim($string), $match)
+			? $match['firstWord']
+			: '';
 	}
 
 
 	public static function removeFirstWord(string $string): string
 	{
-		$string = strstr(trim($string), " ");
+		$string = preg_replace('/^\S+(?:(?=\S+)\s)?/', '', trim($string), 1);
 
 		return $string ? $string : '';
 	}
